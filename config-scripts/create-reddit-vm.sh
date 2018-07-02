a867371238a0f43351766eba5fbd65aec35efcf7
Dmitriy Lushnikov <lushndm@gmail.com>
2018-07-02 13:59:35 +0300

    Added variables and .gitignore. Added ubuntu16.json and immutable.json + puma.service. Added create-reddit-vm.sh.

---
+++ config-scripts/create-reddit-vm.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcloud compute instances create reddit-app-full --project=infra-207413 --zone=europe-west1-b --machine-type=g1-small --subnet=default --no-restart-on-failure --tags=puma-server --image-family=reddit-full --image-project=infra-207413 --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=reddit-app-full

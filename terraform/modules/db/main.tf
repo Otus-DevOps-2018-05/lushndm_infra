443ecfa5c42102de504e840c886f58db222609be
Dmitriy Lushnikov <lushndm@gmail.com>
2018-07-18 17:15:46 +0300

    Added modules app, db and vpc. Deleted app.tf, db.tf and vpc.tf

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "db" {
   name         = "reddit-db"
   machine_type = "g1-small"
-  zone         = "europe-west1-b"
+  zone         = "${var.zone}"
   tags         = ["reddit-db"]
 
   boot_disk {
